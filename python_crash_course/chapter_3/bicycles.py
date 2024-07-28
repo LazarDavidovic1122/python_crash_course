81de8efa7e0ab52369403bd342ed3b4990e64976
Lazar Davidović <109022940+LazarDavidovic1122@users.noreply.github.com>
2024-07-28 16:54:07 +0200

    Add files via upload

---
+++ python_crash_course/chapter_3/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+#Indeksi krecu od nule
+#Ne zaboraviti da posle bicycles[0] stavis .title
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
---
+++ python_crash_course/chapter_3/partial_programs/cars_0_first_version.py
@@ -0,0 +1,3 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() #sortira po abecenom redu, leksikografski
+print(cars)
